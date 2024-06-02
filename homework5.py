09f8c3a64b5f590bfb47cb0b9e5288e05e3d7dc5
AlsuV <165937069+AlsuV@users.noreply.github.com>
2024-06-02 20:42:31 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,10 @@
+
+immutable_var = 1, 'aplle', 3
+print(immutable_var)
+# immutable_var [0]= 66
+# print(immutable_var) #кортеж неизменямый
+
+mutable_list = [2, 'lemon', 4]
+print(mutable_list)
+mutable_list[0] = 66
+print(mutable_list)
